22091cba0c6cbf5b32c4481b0797e16d56b092c7
Maarten de Jonge <mdejonge1990@gmail.com>
2017-05-03 15:49:44 +0200

    update data.py

--- data.py
+++ data.py
@@ -6,20 +6,21 @@ import numpy as np
 
 
 class Data():
-    def __init__(self, folder):
-        self.files = list(self.__load_from_disk(folder))
+    def __init__(self, folder, pattern='*.xml'):
+        self.files = list(self.__load_from_disk(folder, pattern))
 
-    def __load_from_disk(folder):
+    def __load_from_disk(self, folder, pattern):
         """ Load each xml file for the given folder as an etree. """
         parser = etree.XMLParser(ns_clean=True, encoding='utf-8')
 
-        for file in glob(os.path.join(folder, '*.xml')):
+        for file in glob(os.path.join(folder, pattern)):
             with open(file, 'r') as f:
                 xml = etree.fromstring(f.read().encode('utf-8'), parser)
                 yield xml
 
     def __get_label(self, node):
-        pass
+        is_speech = node.attrib['is-speech']
+        return 1 if is_speech == 'true' else 0
 
     def sliding_window(self, n, featurizer):
         """
@@ -32,16 +33,16 @@ class Data():
 
         for xml in self.files:
             nodes = xml.xpath('//text')
-            for node1, node2, node3 in zip(*(nodes[i:] for i in range(n))):
-                X.append(featurizer([node1, node2, node3], nodes))
-                y.append(self.__get_label(node2))
+            for window in zip(*(nodes[i:] for i in range(n))):
+                X.append(featurizer(window, nodes))
+                y.append(self.__get_label(window[round(n / 2)]))
 
-        return np.array(X), np.array(y)
+        return np.array(X, dtype='float32'), np.array(y)
 
 
-def tf_idf_featurizer(self, nodes, all_nodes):
+def tf_idf_featurizer(nodes, all_nodes):
     pass
 
 
-def metadeta_featurizer(self, nodes, _):
-    return [node.attrib[key] for node in nodes for key in sorted(node.attrib)]
+def metadata_featurizer(nodes, _):
+    return [node.attrib[key] for node in nodes for key in sorted(node.attrib) if key != 'is-speech']
