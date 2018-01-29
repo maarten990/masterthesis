fffe588eb4c1e60d42ca9887fe28024cda41a1d6
Maarten de Jonge <mdejonge1990@gmail.com>
2018-01-29 21:43:02 +0100

    finish parameterfile implementation

--- clusterer/src/main/kotlin/cmdline/config.kt
+++ clusterer/src/main/kotlin/cmdline/config.kt
@@ -1,5 +1,6 @@
 package cmdline
 
+import clustering.CharData
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
@@ -45,15 +46,22 @@ class ConfigFormat {
     var clustering: ClusteringMethod? = null
 }
 
-fun parseConfig(path: String) {
+data class Config(var clusteringFunc: (List<String>) -> List<Map<CharData, Int>> = { _ -> listOf(mapOf()) },
+                  var labelingFunc: (List<Map<CharData, Int>>) -> Map<CharData, Int> = { _ -> (mapOf()) })
+
+fun parseConfig(path: String): Config {
     val mapper = ObjectMapper(YAMLFactory())
     val config = mapper.readValue(File(path), ConfigFormat().javaClass)
 
+    val conf = Config()
+
     config.labeling?.dbscan.let {
         if (it != null) {
-            val eps = it.epsilon!!
+            val eps = it.epsilon!!.toFloat()
             val min_pts = it.min_pts!!
             println("Labeling algorithm: dbscan, eps: $eps, min_pts: $min_pts")
+
+            conf.labelingFunc = { files -> labelDbscan(files, eps, min_pts) }
         }
     }
 
@@ -61,6 +69,8 @@ fun parseConfig(path: String) {
         if (it != null) {
             val k = it.k!!
             println("Labeling algorithm: kmeans, k: $k")
+
+            conf.labelingFunc = { blocks -> labelClusters(blocks, k) }
         }
     }
 
@@ -68,14 +78,20 @@ fun parseConfig(path: String) {
         if (it != null) {
             val cutoff = it.cutoff!!
             println("Clustering algorithm: HAC, cutoff: $cutoff")
+
+            conf.clusteringFunc = { files -> cluster_hac(files, cutoff) }
         }
     }
 
     config.clustering?.dbscan.let {
         if (it != null) {
-            val eps = it.epsilon!!
+            val eps = it.epsilon!!.toFloat()
             val min_pts = it.min_pts!!
             println("Clustering algorithm: dbscan, eps: $eps, min_pts: $min_pts")
+
+            conf.clusteringFunc = { files -> cluster_dbscan(files, eps, min_pts) }
         }
     }
+
+    return conf
 }
--- clusterer/src/main/kotlin/cmdline/main.kt
+++ clusterer/src/main/kotlin/cmdline/main.kt
@@ -25,15 +25,10 @@ fun main(args: Array<String>) {
         return Application.launch(gui.ClusterApp::class.java, *args)
     }
 
-    parseConfig(opts["<param_file>"] as String)
-    val blocks = when {
-        opts["dbscan"] == true -> cluster_dbscan(opts["<files>"] as List<String>, (opts["<eps>"] as String).toFloat(),
-                (opts["<min_pts>"] as String).toInt())
-        opts["kmeans"] == true -> listOf(mapOf())
-        else -> listOf(mapOf())
-    }
+    val conf = parseConfig(opts["<param_file>"] as String)
+    val blocks = conf.clusteringFunc(opts["<files>"] as List<String>)
+    val labeled = conf.labelingFunc(blocks)
 
-    val labeled = labelClusters(blocks, 3)
     insertIntoXml(opts["<xml>"] as String, labeled)
     return
 
@@ -62,6 +57,22 @@ fun cluster_dbscan(paths: List<String>, epsilon: Float, minSamples: Int): List<M
     return blocks
 }
 
+fun cluster_hac(paths: List<String>, cutoff: Int): List<Map<CharData, Int>> {
+    val blocks = mutableListOf<Map<CharData, Int>>()
+    // TODO: use all files
+    val doc = PDDocument.load(File(paths[0]))
+    val clusterer = Clusterer()
+
+    for (pagenum in 0 until doc.numberOfPages) {
+        clusterer.vectorizer = Vectorizer.GEOM
+        blocks.add(clusterer.clusterFilePage(doc, pagenum).collectBelowCutoff(cutoff))
+    }
+
+    doc.close()
+
+    return blocks
+}
+
 fun labelClusters(blocks: List<Map<CharData, Int>>, k: Int): Map<CharData, Int> {
     val clusterer = Clusterer()
     clusterer.vectorizer = Vectorizer.ONLY_DIMS
@@ -70,6 +81,16 @@ fun labelClusters(blocks: List<Map<CharData, Int>>, k: Int): Map<CharData, Int> 
     val clusterGroups = blocks.flatMap(::labelMappingToLists)
 
     return clusterer.kmeans(clusterGroups.map(::getBoundingRect), k)
+}
+
+fun labelDbscan(blocks: List<Map<CharData, Int>>, eps: Float, min_pts: Int): Map<CharData, Int> {
+    val clusterer = Clusterer()
+    clusterer.vectorizer = Vectorizer.ONLY_DIMS
+
+    // group the data into lists of chardata objects belonging to the same cluster, for the document as a whole
+    val clusterGroups = blocks.flatMap(::labelMappingToLists)
+
+    return clusterer.dbscan(clusterGroups.map(::getBoundingRect), eps, min_pts)
 }
 
 fun insertIntoXml(path: String, labels: Map<CharData, Int>) {
