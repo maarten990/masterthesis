4699309f39977603f364208f83dd241ca29e180f
Maarten de Jonge <mdejonge1990@gmail.com>
2018-03-19 15:58:22 +0100

    update unit tests

--- supervised/tests/test_data.py
+++ supervised/tests/test_data.py
@@ -2,9 +2,11 @@ import random
 import numpy as np
 from .. import data
 
+import pytest
+
 
 def test_create_dictionary():
-    vocab = data.create_dictionary('training_data', '1800*.xml')
+    vocab = data.create_dictionary(['../clusterlabeled-5/18001.xml'])
 
     # both dictionaries should of course be the same length as they map to
     # one-another
@@ -20,57 +22,53 @@ def test_create_dictionary():
         assert i == reverse_idx
 
 
-def test_sliding_window():
-    X_train, y_is_speech, Y_speaker, vocab = data.sliding_window('tests', 'test.xml', 2, 1)
-
-    # text.xml contains 4 speeches; with a window size of 2 that means there are 3 windows
-    assert len(X_train) == 3
-
-    # test the expected windows
-    expected = ['text 1 text 2', 'text 2 speaker : text 3', 'speaker : text 3 text 4']
-    for X, e in zip(X_train, expected):
-        assert ' '.join(vocab.idx_to_token[x] for x in X) == e
-
-    # the 3rd node contains a speech by 'Mister Speaker'
-    assert np.all(y_is_speech == np.array([0, 0, 1]))
-
-    # test the expected speakers, which are boolean arrays of the same size as
-    # the entries in X_train, indicating wether each tokens is part of the name
-    for i, (speaker, tokens) in enumerate(zip(Y_speaker, X_train)):
-        assert len(speaker) == len(tokens)
-
-        speaker = np.array(speaker)
-        if i == 2:
-            assert speaker[0] == 1 and np.all(speaker[1:] == 0)
-        else:
-            assert np.all(speaker == 0)
+@pytest.fixture(scope='module')
+def dataset():
+    num_clusters = 5
+    num_pos = 10
+    num_neg = 100
+    d = data.GermanDatasetInMemory([f'../clusterlabeled-{num_clusters}/18001.xml',
+                                    f'../clusterlabeled-{num_clusters}/18001.xml'],
+                                   num_clusters, num_pos, num_neg, 3)
+    assert len(d) == num_pos + num_neg
+    return d
 
 
-def test_pad_sequences():
-    # Length: count
-    # 1: 2
-    # 3: 5
-    # 5: 3
-    X = [[0] for _ in range(2)] + [[0, 0, 0] for _ in range(5)] \
-        + [[0, 0, 0, 0, 0] for _ in range(3)]
+def batch_test(batch, vocab):
+    for size, s in batch.items():
+        data = s['data']
+        assert data.shape[-1] == size
 
-    random.shuffle(X)
+        if len(data.shape) == 1:
+            data = np.expand_dims(data, 0)
 
-    # labels are the lengths of the sequences
-    y = [len(seq) for seq in X]
+        for i in range(data.shape[0]):
+            tokens = [vocab.idx_to_token.get(idx, '<unknown>') for idx in data[i, :]]
+            assert s['label'][i] == 0 or ':' in tokens
 
-    sizes = [3, -1]
-    buckets, labels = data.pad_sequences(X, y, sizes)
-    assert len(buckets) == len(sizes)
-    assert len(buckets) == len(labels)
-    assert buckets[0].shape == (7, sizes[0])
-    assert buckets[1].shape == (3, 5)
-    for bucket, label in zip(buckets, labels):
-        assert np.all(label <= (bucket.shape[1]))
 
-    sizes = [2, 4]
-    buckets, labels = data.pad_sequences(X, y, sizes)
-    assert len(buckets) == len(sizes)
-    assert len(buckets) == len(labels)
-    assert buckets[0].shape == (2, sizes[0])
-    assert buckets[1].shape == (8, sizes[1])
+def test_dataset(dataset):
+    for sample in dataset:
+        batch_test(sample, dataset.vocab)
+
+
+def test_dataloader(dataset):
+    buckets = [10, 20, 40]
+    batch_size = 32
+    dl = data.get_iterator(dataset, buckets=buckets, batch_size=batch_size)
+    not_batch_size = 0
+    for batch in dl:
+        batch_test(batch, dataset.vocab)
+        size_of_batch = 0
+        for size, s in batch.items():
+            assert size in buckets
+            size_of_batch += s['data'].shape[0]
+
+        if size_of_batch != batch_size:
+            not_batch_size += 1
+
+    assert not_batch_size <= 1
+
+
+def test_split(dataset):
+    assert False
